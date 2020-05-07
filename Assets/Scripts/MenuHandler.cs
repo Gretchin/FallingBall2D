dbc32070409ecd1b61df179de0fa7825f63a40ee
Aleksandr Urusovskiy <alexander.u@fx.gl>
2020-05-07 20:59:04 +0300

    Вспомнил про гит. Добавил сцену меню с 2 кнопками. Переход на сцену Игры, где шар падает на блок

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+
+  public void Play()
+  {
+    Debug.Log("Play pressed");
+    SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+  }
+
+  public void Exit()
+  {
+    Debug.Log("Exit pressed");
+    Application.Quit();
+  }
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
