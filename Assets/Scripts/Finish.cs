ad9973440d97952099c9470cd8d9222e131667e6
Aleksandr Urusovskiy <alexander.u@fx.gl>
2020-05-10 15:46:02 +0300

    GameObject Финиш

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+  public GameObject finish;
+
+  void Start()
+  {
+    Debug.Log("FISIN");
+
+  }
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.gameObject == finish)
+    {
+      Debug.Log("trigger");
+    }
+  }
+}
