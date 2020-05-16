6e2a747d9adb859bfcb95d7bfb77a05ad762b169
Aleksandr Urusovskiy <alexander.u@fx.gl>
2020-05-16 13:47:33 +0300

    Поправил баг с вытаскиванием предметов из инвентаря во время симуляции

--- Assets/Scripts/InvItemDragHandler.cs
+++ Assets/Scripts/InvItemDragHandler.cs
@@ -4,17 +4,24 @@ using UnityEngine.EventSystems;
 public class InvItemDragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
   public GameObject gameItem;
+  GameHandler gameHandler;
+
+  void Start()
+  {
+    gameHandler = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameHandler>();
+  }
 
   void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
   {
-    if (gameItem)
+    if (gameHandler.IsSimulated() || !gameItem)
     {
-      // Vector2 чтобы обнулить координату z
-      Vector2 newGameItemPosition = Camera.main.ScreenToWorldPoint(gameObject.transform.position);
-      GameObject newItem = Instantiate(gameItem, newGameItemPosition, Quaternion.identity);
-      newItem.GetComponent<DragAndRotating>()?.SetIsMoving(true);
-      Destroy(gameObject);
+      return;
     }
+    // Vector2 чтобы обнулить координату z
+    Vector2 newGameItemPosition = Camera.main.ScreenToWorldPoint(gameObject.transform.position);
+    GameObject newItem = Instantiate(gameItem, newGameItemPosition, Quaternion.identity);
+    newItem.GetComponent<DragAndRotating>()?.SetIsMoving(true);
+    Destroy(gameObject);
   }
   public void OnDrag(PointerEventData eventData) { }
   public void OnEndDrag(PointerEventData eventData) { }
